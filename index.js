de16c7e59a597931e8c7646a8df21f813de34bec
Ralf Schimmel <r.schimmel@gynzy.com>
2016-07-18 20:43:28 +0200

    Use es7 async await style with node-then

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 'use strict';
 
-const redis = require('redis'),
-    co = require('co'),
-    coRedis = require('co-redis'),
-    Koa = require('koa');
+const redis = require('then-redis'),
+	Koa = require('koa');
+
+const redisOptions = {
+	host: process.env.REDIS_HOST,
+	port: process.env.REDIS_PORT,
+	password: process.env.REDIS_SECRET
+};
 
 const app = exports.app = new Koa(),
-    client  = redis.createClient(
-      process.env.REDIS_PORT,
-      process.env.REDIS_HOST
-    ),
-    dbCo = coRedis(client);
-
-if (process.env.REDIS_SECRET) {
-  client.auth(process.env.REDIS_SECRET);
-}
+	client = redis.createClient(redisOptions);
 
 client.on('error', function (err) {
-  console.log('Redis client error: ' + err);
+	console.log('Redis client error: ' + err);
 });
 
-app.use(co.wrap(function* (ctx) {
+app.use(async ctx => {
 
-  var indexkey;
+	var indexkey;
 
-  if (ctx.request.query.index_key) {
-    indexkey = process.env.APP_NAME +':'+ ctx.request.query.index_key;
-  } else {
-    indexkey = process.env.APP_NAME +':current-content';
-  }
-  var index = yield dbCo.get(indexkey);
+	if (ctx.query.index_key) {
+		indexkey = process.env.APP_NAME + ':index:' + ctx.query.index_key;
+	} else {
+		indexkey = process.env.APP_NAME + ':index:current-content';
+	}
 
-  if (index) {
-    ctx.body = index;
-  } else {
-    ctx.status = 404;
-  }
-}));
+	var index = await client.get(indexkey);
+	if (index) {
+		ctx.body = index;
+	} else {
+		ctx.status = 404;
+	}
+});
 
-var server = app.listen(process.env.PORT || 3000);
+console.log('Starting server for: ', process.env.APP_NAME);
+var port = process.env.PORT || 3000;
+var server = app.listen(port);
+console.log('Server running on port ', port);
 
 process.on('SIGINT', function () {
-  server.close(function () {
-    process.exit(0);
-  });
+	server.close(function () {
+		process.exit(0);
+	});
 });
